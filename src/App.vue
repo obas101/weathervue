c3252430626012a519f329ae906937b784a3b348
Yunusa Onivehu <obadobay@gmail.com>
2020-10-31 10:40:50 +0100

    first commit front end

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="app">
+		<main>
+			<div class="body-wrapper">
+				<div class="input-container">
+					<input type="text" placeholder="Enter Location or City..." />
+				</div>
+				<div class="main-display"></div>
+			</div>
+		</main>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+};
 </script>
 
 <style>
+* {
+	padding: 0;
+	margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	background-image: url(./assets/back.jpg);
+	height: 100vh;
+	background-size: cover;
+}
+.input-container {
+	width: 100%;
+}
+input {
+	width: 100%;
+	padding: 1rem;
+	outline: none;
+	border-radius: 30px;
+	font-size: 1.3rem;
+  padding-left: 2rem;
+}
+main {
+	padding-top: 4rem;
+	padding-left: 9rem;
+	padding-right: 9rem;
+	min-height: 100vh;
 }
 </style>
